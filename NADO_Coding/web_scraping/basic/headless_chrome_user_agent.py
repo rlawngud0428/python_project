6f673a38367b3dc332089ab41bbc5b88b76fc1c5
rlawngud0428 <55781022+rlawngud0428@users.noreply.github.com>
2021-01-14 22:48:33 +0900

    Update headless_chrome_user_agent.py

--- NADO_Coding/web_scraping/basic/headless_chrome_user_agent.py
+++ NADO_Coding/web_scraping/basic/headless_chrome_user_agent.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.headless = True
 options.add_argument("window-size=1920x1080")
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+options.add_argument("user-agent=[user agent string]")
 
 browser = webdriver.Chrome(options=options)
 browser.maximize_window()
